cfdc409b0ad1abf6eacdf3757a0254ec25847dd6
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-04-17 16:44:05 +0300

    create EditTaskPage

---
+++ src/test/java/pageobjects/EditTaskPage.java
@@ -0,0 +1,89 @@
+package pageobjects;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EditTaskPage extends AbstractPage {
+    private WebDriver driver;
+
+    EditTaskPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//div[1]/h2")
+    private WebElement headingEditTask;
+    @FindBy(xpath = "//tr[1]/td[1]")
+    private WebElement labelName;
+    @FindBy(xpath = "//tr[2]/td[1]")
+    private WebElement labelDescription;
+    @FindBy(xpath = "//tr[3]/td[1]")
+    private WebElement labelResponsible;
+    @FindBy(xpath = "//input[@name='name']")
+    private WebElement inputName;
+    @FindBy(xpath = "//textarea[@name='description']")
+    private WebElement inputDescription;
+    @FindBy(xpath = "//select[@class='form-control']")
+    private WebElement dropBoxResponsible;
+    @FindBy(xpath = "//input[@type='submit']")
+    private WebElement buttonChangeTask;
+
+    public String getHeadingAddTaskNameText() {
+        return headingEditTask.getText();
+    }
+
+    public String getLabelNameText() {
+        return labelName.getText();
+    }
+
+    public String getLabelDescriptionNameText() {
+        return labelDescription.getText();
+    }
+
+    public String getLabelResponsibleNameText() {
+        return labelResponsible.getText();
+    }
+
+    public EditTaskPage editName(String name) {
+        inputName.clear();
+        inputName.sendKeys(name);
+        return this;
+    }
+
+    public EditTaskPage editDescription(String description) {
+        inputDescription.clear();
+        inputDescription.sendKeys(description);
+        return this;
+    }
+
+    public EditTaskPage editResponsible(String responsible) {
+        dropBoxResponsible.click();
+        dropBoxResponsible.sendKeys(responsible);
+        dropBoxResponsible.sendKeys(Keys.ENTER);
+        return this;
+    }
+    /* public TasksPage clickChangeTask() {
+        buttonChangeTask.click();
+        return new TasksPage(driver);
+    } */
+
+    public EditTaskPage changeAllFillingFormAddTask(String name, String description, String responsible) {
+        editName(name);
+        editDescription(description);
+        editResponsible(responsible);
+        return this;
+    }
+
+   /*  public TasksPage ChangeFormCreateEditTask(String name, String description, String responsible) {
+        editName(name);
+        editDescription(description);
+        editResponsible(responsible);
+        clickChangeTask();
+        return new TasksPage(driver);
+}*/
+
+}
+
