6b5b1a589d8b37e11ab1f09cdb7c8d1de4836759
trainingby1 <trainingby1@google.com>
2022-04-21 17:26:39 +0300

    add AbstractTest

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AbstractTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+abstract class AbstractTest {
+    static WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 }
