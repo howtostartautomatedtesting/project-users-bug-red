98283d5289929e9bc1141659699faae0c7ee9ab2
RamanBohdan <lo63iksport004@gmail.com>
2022-04-23 02:29:26 +0300

    refactor TitleAddTaskPage

--- src/test/java/title/TitleAddTaskPage.java
+++ src/test/java/title/TitleAddTaskPage.java
@@ -6,6 +6,6 @@ public class TitleAddTaskPage {
     public static final String EXPECTED_FIELD_TASK_NAME_TITLE = "Название задачи";
     public static final String EXPECTED_FIELD_RESPONSIBLE_TITLE = "Ответственный";
     public static final String EXPECTED_FIELD_DESCRIPTION_TITLE = "Описание задачи";
-    public static final String EXPECTED_BUTTON_ADD_TASK_TITLE = "Добавить задачу";
+    public static final String EXPECTED_BUTTON_ADD_TASK_TITLE = "";
 
 }
