28378985b109e7bdf59fdbb791087fc901606771
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-04-22 22:03:27 +0300

    refactor method name

--- src/test/java/TasksPageTest.java
+++ src/test/java/TasksPageTest.java
@@ -12,11 +12,9 @@ public class TasksPageTest extends AbstractTest {
         TasksPage tasksPage = new UserLoginPage(driver).openPage()
                 .fillFormRegistration(UserCreator.getUserName(),UserCreator.getEmail(),UserCreator.getPassword())
                 .clickButtonTasks();
-
         Assert.assertEquals(tasksPage.getPageTitle(), TasksPageUITitles.EXPECTED_PAGE_TITLE);
         Assert.assertEquals(tasksPage.getColumnTitleTaskName(), TasksPageUITitles.EXPECTED_COLUMN_TASK_NAME_TITLE);
         Assert.assertEquals(tasksPage.getColumnTitleSchedule(), TasksPageUITitles.EXPECTED_COLUMN_SCHEDULE_TITLE);
         Assert.assertEquals(tasksPage.getButtonAddTaskName(), TasksPageUITitles.EXPECTED_BUTTON_ADD_TASK_TITLE);
     }
-
 }
--- src/test/java/uiTitles/TasksPageUITitles.java
+++ src/test/java/uiTitles/TasksPageUITitles.java
@@ -5,6 +5,4 @@ public class TasksPageUITitles {
     public static final String EXPECTED_COLUMN_TASK_NAME_TITLE = "Название";
     public static final String EXPECTED_COLUMN_SCHEDULE_TITLE = "Расписание";
     public static final String EXPECTED_BUTTON_ADD_TASK_TITLE = "Добавить задачу";
-
-
 }
