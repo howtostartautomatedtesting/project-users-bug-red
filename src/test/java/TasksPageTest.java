50b4d77aeaabb5dbd8114fcae12b01186eeaa916
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-04-22 10:42:31 +0300

    refactor HomePageTest

--- src/test/java/TasksPageTest.java
+++ src/test/java/TasksPageTest.java
@@ -2,6 +2,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageobjects.TasksPage;
 import pageobjects.UserLoginPage;
+import uiTitles.TasksPageUITitles;
 
 public class TasksPageTest extends AbstractTest {
 
@@ -11,10 +12,10 @@ public class TasksPageTest extends AbstractTest {
                 .fillFormLogin("barsov86@yandex.ru", "123")
                 .clickButtonTasks();
 
-        Assert.assertEquals(tasksPage.getPageTitle(), "Задачи");
-        Assert.assertEquals(tasksPage.getColumnTitleTaskName(), "Название");
-        Assert.assertEquals(tasksPage.getColumnTitleSchedule(), "Расписание");
-        Assert.assertEquals(tasksPage.getButtonAddTaskName(), "Добавить задачу");
+        Assert.assertEquals(tasksPage.getPageTitle(), TasksPageUITitles.EXPECTED_PAGE_TITLE);
+        Assert.assertEquals(tasksPage.getColumnTitleTaskName(), TasksPageUITitles.EXPECTED_COLUMN_TASK_NAME_TITLE);
+        Assert.assertEquals(tasksPage.getColumnTitleSchedule(), TasksPageUITitles.EXPECTED_COLUMN_SCHEDULE_TITLE);
+        Assert.assertEquals(tasksPage.getButtonAddTaskName(), TasksPageUITitles.EXPECTED_BUTTON_ADD_TASK_TITLE);
     }
 
 }
---
+++ src/test/java/uiTitles/TasksPageUITitles.java
@@ -0,0 +1,10 @@
+package uiTitles;
+
+public class TasksPageUITitles {
+    public static final String EXPECTED_PAGE_TITLE = "Задачи";
+    public static final String EXPECTED_COLUMN_TASK_NAME_TITLE = "Название";
+    public static final String EXPECTED_COLUMN_SCHEDULE_TITLE = "Расписание";
+    public static final String EXPECTED_BUTTON_ADD_TASK_TITLE = "Добавить задачу";
+
+
+}
