fd1e56d058f2d20452465d900662f2c7c30f5347
Iryna Ihnatsenka <irysi4ka.92@mail.ru>
2022-04-23 10:38:06 +0300

    add new assert

--- src/test/java/UserFullProfilePageTest.java
+++ src/test/java/UserFullProfilePageTest.java
@@ -4,8 +4,6 @@ import pageobjects.UserFullProfilePage;
 
 public class UserFullProfilePageTest extends AbstractTest {
 
-
-
     @Test
     public void testOpenPage() throws InterruptedException {
         UserFullProfilePage userFullProfilePage = new UserFullProfilePage(driver);
@@ -13,21 +11,41 @@ public class UserFullProfilePageTest extends AbstractTest {
         userFullProfilePage.openUsersPage();
         Assert.assertEquals(userFullProfilePage.getHeadingProfile(), "Профиль пользователя");
         Assert.assertEquals(userFullProfilePage.getLabelEmailText(), "Email");
-        Assert.assertEquals(userFullProfilePage.getLabelInitialsText(), "ФИО");
-        Assert.assertEquals(userFullProfilePage.getLabelGenderText(), "Пол");
-        Assert.assertEquals(userFullProfilePage.getLabelBirthdayText(), "Дата рождения");
-        Assert.assertEquals(userFullProfilePage.getLabelDateStartText(), "Начал работать в компании");
-        Assert.assertEquals(userFullProfilePage.getLabelHobbyText(), "Хобби");
-        Assert.assertEquals(userFullProfilePage.getLabelINNText(), "ИНН");
         Assert.assertEquals(userFullProfilePage.getEnteredEmail(), "ira@mail.ru");
+        Assert.assertEquals(userFullProfilePage.getLabelInitialsText(), "ФИО");
         Assert.assertEquals(userFullProfilePage.getEnteredInitials(), "Ирусик");
+        Assert.assertEquals(userFullProfilePage.getLabelGenderText(), "Пол");
         // Assert.assertEquals(userFullProfilePage.getSelectedGender(), "Не определен"); выбивает ошибку
+        Assert.assertEquals(userFullProfilePage.getLabelBirthdayText(), "Дата рождения");
         Assert.assertEquals(userFullProfilePage.getEnteredBirthday(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelDateStartText(), "Начал работать в компании");
         Assert.assertEquals(userFullProfilePage.getEnteredDateStart(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelHobbyText(), "Хобби");
         Assert.assertEquals(userFullProfilePage.getEnteredHobby(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelNameOne(), "имя1");
+        Assert.assertEquals(userFullProfilePage.getInputNameOne(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelSurnameOne(), "фамилия1");
+        Assert.assertEquals(userFullProfilePage.getInputSurnameOne(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelPatronymicOne(), "отчество1");
+        Assert.assertEquals(userFullProfilePage.getInputPatronymicOne(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelCat(), "Кошечка");
+        Assert.assertEquals(userFullProfilePage.getInputCat(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelDog(), "Собачка");
+        Assert.assertEquals(userFullProfilePage.getInputDog(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelParrot(), "Попугайчик");
+        Assert.assertEquals(userFullProfilePage.getInputParrot(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelCavy(), "Морская свинка");
+        Assert.assertEquals(userFullProfilePage.getInputCavy(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelHamster(), "Хомячок");
+        Assert.assertEquals(userFullProfilePage.getInputHamster(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelSquirrel(), "Белочка");
+        Assert.assertEquals(userFullProfilePage.getInputSquirrel(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelPhone(), "Телефон");
+        Assert.assertEquals(userFullProfilePage.getInputPhone(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelAddress(), "Адрес");
+        Assert.assertEquals(userFullProfilePage.getInputAddress(), "");
+        Assert.assertEquals(userFullProfilePage.getLabelINNText(), "ИНН");
         Assert.assertEquals(userFullProfilePage.getEnteredINN(), "");
-
-
 
         Thread.sleep(1000);
     }
