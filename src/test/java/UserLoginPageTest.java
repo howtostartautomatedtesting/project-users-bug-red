8210e28d5fc617efb53ed409daaa67f4b8774e6b
Natallia Butsko <n.butsko92@gmail.com>
2022-04-23 15:59:45 +0300

    resolve conflict in UserLoginPageTest

--- src/test/java/UserLoginPageTest.java
+++ src/test/java/UserLoginPageTest.java
@@ -1,9 +1,6 @@
 import org.testng.annotations.Test;
 import pageobjects.UserLoginPage;
-<<<<<<< HEAD
 import uiTitles.UserLoginPageUITitles;
-=======
->>>>>>> 35915db2d6a4a19426cde63e8e95d7e79e4464ce
 
 import static org.testng.Assert.assertEquals;
 
@@ -11,7 +8,6 @@ public class UserLoginPageTest extends AbstractTest {
 
     @Test
     public void testUserLoginPageFormNames() {
-<<<<<<< HEAD
         UserLoginPage userLoginPage = new UserLoginPage(driver).openPage();
 
         assertEquals(userLoginPage.getNameTitleLogin(), UserLoginPageUITitles.EXPECTED_FORM_LOGIN_TITLE);
@@ -23,26 +19,5 @@ public class UserLoginPageTest extends AbstractTest {
         assertEquals(userLoginPage.getTitlePasswordRegistration(), UserLoginPageUITitles.EXPECTED_FIELD_PASSWORD_TITLE);
         assertEquals(userLoginPage.getButtonAuthorizationName(), UserLoginPageUITitles.EXPECTED_BUTTON_AUTHORIZATION_TITLE);
         assertEquals(userLoginPage.getButtonRegistrationName(), UserLoginPageUITitles.EXPECTED_BUTTON_REGISTRATION_TITLE);
-=======
-        String expectedFormLoginTitle = "Вход";
-        String expectedFormRegistrationTitle = "Регистрация";
-        String expectedButtonAuthorizationTitle = "Авторизоваться";
-        String expectedButtonRegistrationTitle = "Зарегистрироваться";
-        String expectedFieldNameTitle = "Имя";
-        String expectedFieldEmailTitle = "Email";
-        String expectedFieldPasswordTitle = "Пароль";
-
-        UserLoginPage userLoginPage = new UserLoginPage(driver).openPage();
-
-        assertEquals(userLoginPage.getNameTitleLogin(), expectedFormLoginTitle);
-        assertEquals(userLoginPage.getNameTitleRegistration(), expectedFormRegistrationTitle);
-        assertEquals(userLoginPage.getTitleEmailLogin(), expectedFieldEmailTitle);
-        assertEquals(userLoginPage.getTitlePasswordLogin(), expectedFieldPasswordTitle);
-        assertEquals(userLoginPage.getTitleNameRegistration(), expectedFieldNameTitle);
-        assertEquals(userLoginPage.getTitleEmailRegistration(), expectedFieldEmailTitle);
-        assertEquals(userLoginPage.getTitlePasswordRegistration(), expectedFieldPasswordTitle);
-        assertEquals(userLoginPage.getButtonAuthorizationName(), expectedButtonAuthorizationTitle);
-        assertEquals(userLoginPage.getButtonRegistrationName(), expectedButtonRegistrationTitle);
->>>>>>> 35915db2d6a4a19426cde63e8e95d7e79e4464ce
     }
 }
