87c487c3dd5cf866898475569b0c7dee9247544c
Alexandr Drankevich <92778866+AlexandrDrankevich@users.noreply.github.com>
2022-04-23 13:02:13 +0300

    TasksPageTest
    * add testUITasksPage
    
    * refactor HomePageTest
    
    * add UserCreator
    
    * refactor method name
    
    * refactor method name
    
    * refactor method name in pageObject

---
+++ src/test/java/uiTitles/TasksPageUITitles.java
@@ -0,0 +1,8 @@
+package uiTitles;
+
+public class TasksPageUITitles {
+    public static final String EXPECTED_PAGE_TITLE = "Задачи";
+    public static final String EXPECTED_COLUMN_TASK_NAME_TITLE = "Название";
+    public static final String EXPECTED_COLUMN_SCHEDULE_TITLE = "Расписание";
+    public static final String EXPECTED_BUTTON_ADD_TASK_TITLE = "Добавить задачу";
+}
