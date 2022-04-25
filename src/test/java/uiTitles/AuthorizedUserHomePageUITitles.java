8a759849b16a8477d5ae5dc9f0cdcd7db21b2017
Irysik <36077989+Irysi4ka@users.noreply.github.com>
2022-04-25 16:32:33 +0300

    add iuTitles

---
+++ src/test/java/uiTitles/AuthorizedUserHomePageUITitles.java
@@ -0,0 +1,9 @@
+package uiTitles;
+
+public class AuthorizedUserHomePageUITitles {
+    public static final String EXPECTED_BUTTON_USERS_TITLE = "Пользователи";
+    public static final String EXPECTED_BUTTON_TASKS_TITLE = "Задачи";
+    public static final String EXPECTED_BUTTON_COMPANIES_TITLE = "Компании";
+    public static final String EXPECTED_BUTTON_NOTIFICATIONS_TITLE = " 0";
+    public static final String EXPECTED_BUTTON_USER_TASKS_TITLE = "0 заданий";
+}
