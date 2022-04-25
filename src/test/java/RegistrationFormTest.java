947af662356735eb0930587d40df78d525598adc
Irysik <36077989+Irysi4ka@users.noreply.github.com>
2022-04-25 21:20:37 +0300

    add testRegistrationWithEmptyName

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -8,17 +8,25 @@ public class RegistrationFormTest extends AbstractTest {
     @Test
     public void testRegistrationWithEmptyPassword() throws InterruptedException {
         userLoginPage = new UserLoginPage(driver).openPage();
-        userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(), UserCreator.getEmail(), "" );
+        userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(), UserCreator.getEmail(), "");
         userLoginPage.clickButtonRegistration();
 
         Thread.sleep(3000);
     }
 
     @Test
-    public void testRegistrationWithEmptyEmail(){
+    public void testRegistrationWithEmptyEmail() {
         userLoginPage = new UserLoginPage(driver).openPage();
-        userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(),"", UserCreator.getPassword());
+        userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(), "", UserCreator.getPassword());
         userLoginPage.clickButtonRegistration();
     }
-    
+
+    @Test
+    public void testRegistrationWithEmptyName() {
+        userLoginPage = new UserLoginPage(driver).openPage();
+        userLoginPage.fillFormRegistrationAndClickButtonRegistration("", UserCreator.getEmail(), UserCreator.getPassword());
+        userLoginPage.clickButtonRegistration();
+    }
+
+
 }
