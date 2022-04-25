93d4a246502046d384f5684875f065ae09455047
Irysik <36077989+Irysi4ka@users.noreply.github.com>
2022-04-25 21:34:35 +0300

    add testRegistrationWithValidDate

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -1,15 +1,18 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pageobjects.AuthorizedUserHomePage;
 import pageobjects.UserLoginPage;
 import utils.UserCreator;
 
 public class RegistrationFormTest extends AbstractTest {
     UserLoginPage userLoginPage;
+    AuthorizedUserHomePage authorizedUserHomePage;
+
 
     @Test
     public void testRegistrationWithEmptyPassword() throws InterruptedException {
         userLoginPage = new UserLoginPage(driver).openPage();
         userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(), UserCreator.getEmail(), "");
-        userLoginPage.clickButtonRegistration();
 
         Thread.sleep(3000);
     }
@@ -18,15 +21,23 @@ public class RegistrationFormTest extends AbstractTest {
     public void testRegistrationWithEmptyEmail() {
         userLoginPage = new UserLoginPage(driver).openPage();
         userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(), "", UserCreator.getPassword());
-        userLoginPage.clickButtonRegistration();
     }
 
     @Test
     public void testRegistrationWithEmptyName() {
         userLoginPage = new UserLoginPage(driver).openPage();
         userLoginPage.fillFormRegistrationAndClickButtonRegistration("", UserCreator.getEmail(), UserCreator.getPassword());
-        userLoginPage.clickButtonRegistration();
     }
 
+    @Test
+    public void testRegistrationWithValidData() throws InterruptedException {
+        userLoginPage = new UserLoginPage(driver).openPage();
+        authorizedUserHomePage = new AuthorizedUserHomePage(driver);
+
+        userLoginPage.fillFormRegistrationAndClickButtonRegistration(UserCreator.getUserName(), UserCreator.getEmail(), UserCreator.getPassword());
+        Assert.assertEquals(authorizedUserHomePage.getDropdownMenuUserAccountName(), UserCreator.getUserName().toLowerCase());
+
+        Thread.sleep(3000);
+    }
 
 }
