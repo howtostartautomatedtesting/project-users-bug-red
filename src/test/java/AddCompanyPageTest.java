2eaa594aeae9a42c8f02d5607f96497ada1b4dcb
Natallia Butsko <n.butsko92@gmail.com>
2022-04-27 18:41:47 +0300

    add test method in AddCompanyPageTest for add company with empty fields
    resolve conflict

--- src/test/java/AddCompanyPageTest.java
+++ src/test/java/AddCompanyPageTest.java
@@ -42,7 +42,22 @@ public class AddCompanyPageTest extends AbstractTest {
     }
 
     @Test(groups = "authorizedUser", enabled = false)
-    public void testAddCompanyPageFieldsTypes(){
+    public void testAddCompanyPageFieldsTypes() {
         //надо ли проверять типы полей формы (text, button, select...)?
     }
+
+    @Test(groups = "authorizedUser")
+    public void testAddCompanyPageAddCompanyWithEmptyFields() {
+        String emptyField = "";
+        AddCompanyPage addCompanyPage = authorizedUser.clickButtonCompanies()
+                .clickButtonAddCompany()
+                .fillFormAddCompany(emptyField, emptyField, emptyField, emptyField, emptyField, emptyField, emptyField, emptyField);
+
+        //проверка для русской локализации браузера
+        assertEquals(addCompanyPage.getMessageIfFieldNameIsEmpty(), AddCompanyPage.MESSAGE_EMPTY_INPUT_FIELD_RUS);
+
+        //проверка для английской локализации браузера
+        //assertEquals(addCompanyPage.getMessageIfFieldNameIsEmpty(), AddCompanyPage.MESSAGE_EMPTY_INPUT_FIELD_ENG);
+    }
+// (add test method in AddCompanyPageTest for add company with empty fields)
 }
--- src/test/java/pageobjects/AddCompanyPage.java
+++ src/test/java/pageobjects/AddCompanyPage.java
@@ -20,6 +20,9 @@ public class AddCompanyPage extends AbstractPage {
     public static final String BUTTON_LABEL_CLEAR_FIELD_USERS = "Очистить поле";
     public static final String BUTTON_LABEL_ADD_COMPANY = "Добавить компанию";
 
+    public static final String MESSAGE_EMPTY_INPUT_FIELD_ENG = "Please fill out this field.";
+    public static final String MESSAGE_EMPTY_INPUT_FIELD_RUS = "Заполните это поле.";
+
     public static final String ADD_PAGE_COMPANY_URL = BASE_URL + "/companys/add.html";
 
     private WebDriver driver;
@@ -28,8 +31,10 @@ public class AddCompanyPage extends AbstractPage {
     private WebElement inputName;
     @FindBy(xpath = "//h2[text()='Добавление компании']")
     private WebElement labelHeaderTitle;
-    @FindBy(xpath = "//select[@name='type']")
+    @FindBy(xpath = "//span[@class='select2-selection__arrow']")
     private WebElement selectCompany;
+    @FindBy(xpath = "//span/span[1]/input")
+    private WebElement inputTypeCompany;
     @FindBy(xpath = "//input[@name='inn']")
     private WebElement inputInn;
     @FindBy(xpath = "//input[@name='ogrn']")
@@ -40,7 +45,7 @@ public class AddCompanyPage extends AbstractPage {
     private WebElement inputPhoneNumber;
     @FindBy(xpath = "//input[@name='adress']")
     private WebElement inputAddress;
-    @FindBy(xpath = "//ul[@class='select2-selection__rendered']")
+    @FindBy(xpath = "//input[@class='select2-search__field']")
     private WebElement selectEmployees;
     @FindBy(xpath = "//a[@class='clear_field']")
     private WebElement buttonClearField;
@@ -57,7 +62,7 @@ public class AddCompanyPage extends AbstractPage {
         return this;
     }
 
-    public String getHeaderTitle(){
+    public String getHeaderTitle() {
         return labelHeaderTitle.getText();
     }
 
@@ -68,8 +73,7 @@ public class AddCompanyPage extends AbstractPage {
 
     public AddCompanyPage chooseType(String type) {
         selectCompany.click();
-        selectCompany.sendKeys(type);
-        selectCompany.sendKeys(Keys.ENTER);
+        inputTypeCompany.sendKeys(type, Keys.ENTER);
         return this;
     }
 
@@ -121,7 +125,7 @@ public class AddCompanyPage extends AbstractPage {
         return this;
     }
 
-    public AddCompanyPage fillFormAndClickAddCompany(String name, String type, String inn, String ogrn, String kpp, String number, String address, String user) {
+    public CompaniesPage fillFormAndClickAddCompany(String name, String type, String inn, String ogrn, String kpp, String number, String address, String user) {
         typeName(name);
         chooseType(type);
         typeInn(inn);
@@ -131,51 +135,55 @@ public class AddCompanyPage extends AbstractPage {
         typeAddress(address);
         chooseEmployees(user);
         clickAddCompany();
-        return this;
+        return new CompaniesPage(driver);
     }
 
-    private String getFieldTitleXPathByNameAttribute(String valueOfAttributeName){
+    private String getFieldTitleXPathByNameAttribute(String valueOfAttributeName) {
         String defaultNameLocator = "//*[@name='%s']/../preceding-sibling::td";
         return String.format(defaultNameLocator, valueOfAttributeName);
     }
 
-    public String getFieldNameTitle(){
+    public String getFieldNameTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("name"))).getText();
     }
 
-    public String getFieldTypeTitle(){
+    public String getFieldTypeTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("type"))).getText();
     }
 
-    public String getFieldINNTitle(){
+    public String getFieldINNTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("inn"))).getText();
     }
 
-    public String getFieldOGRNTitle(){
+    public String getFieldOGRNTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("ogrn"))).getText();
     }
 
-    public String getFieldKPPTitle(){
+    public String getFieldKPPTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("kpp"))).getText();
     }
 
-    public String getFieldPhoneTitle(){
+    public String getFieldPhoneTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("phone"))).getText();
     }
 
-    public String getFieldAddressTitle(){
+    public String getFieldAddressTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("adress"))).getText();
     }
 
-    public String getFieldUsersTitle(){
+    public String getFieldUsersTitle() {
         return driver.findElement(By.xpath(getFieldTitleXPathByNameAttribute("users[]"))).getText();
     }
 
-    public String getButtonClearFieldUsersName(){
+    public String getButtonClearFieldUsersName() {
         return buttonClearField.getText();
     }
 
-    public String getButtonAddCompanyName(){
+    public String getButtonAddCompanyName() {
         return buttonAddCompany.getAttribute("value");
+    }
+
+    public String getMessageIfFieldNameIsEmpty() {
+        return inputName.getAttribute("validationMessage");
     }
 }
