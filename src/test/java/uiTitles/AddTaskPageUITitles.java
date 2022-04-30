85b3bfd0e57c36256f8cb7156090ac1ce4f77894
trainingby1 <trainingby1@google.com>
2022-04-30 17:52:38 +0300

    refactor add task page po & test

--- src/test/java/uiTitles/AddTaskPageUITitles.java
+++
@@ -1,9 +0,0 @@
-package uiTitles;
-
-public class AddTaskPageUITitles {
-    public static final String EXPECTED_PAGE_ADD_HEADING_TITLE = "Добавление задачи";
-    public static final String EXPECTED_PAGE_ADD_TASK_NAME_TITLE = "Название задачи";
-    public static final String EXPECTED_PAGE_ADD_TASK_DESCRIPTION_TITLE = "Описание задачи";
-    public static final String EXPECTED_PAGE_ADD_RESPONSIBLE_TITLE = "Ответственный";
-}
-
