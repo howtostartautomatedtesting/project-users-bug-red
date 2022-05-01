a94001de597fc733bfdc095242e86252480ffc86
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-01 11:31:05 +0300

    add test

--- src/test/java/pageobjects/UserProfilePage.java
+++ src/test/java/pageobjects/UserProfilePage.java
@@ -93,6 +93,8 @@ public class UserProfilePage extends AbstractPage {
         return labelINN.getText();
     }
 
+    public String getInputGender() {return inputGenderM.getText();}
+
     public UserProfilePage changePhoto(String path) {
         buttonLoadPhoto.click();
         buttonLoadPhoto.sendKeys(path);
