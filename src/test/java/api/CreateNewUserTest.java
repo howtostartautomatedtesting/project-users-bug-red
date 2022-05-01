0b6aa8f48f16c690f88f1685802bcde02e86aa50
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-01 16:09:14 +0300

    add test

--- src/test/java/api/CreateNewUserTest.java
+++ src/test/java/api/CreateNewUserTest.java
@@ -1,7 +1,46 @@
 package api;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.UserCreator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *  POST http://users.bugred.ru/tasks/rest/doregister
  */
 public class CreateNewUserTest {
+    private final String userName=UserCreator.getUserName();
+    private final String userEmail=UserCreator.getEmail();
+    private final String userPassword=UserCreator.getPassword();
+    @Test
+    public void testCreateNewUserWithValidData() throws IOException {
+        HttpPost request = new HttpPost("http://users.bugred.ru/tasks/rest/doregister");
+        List<NameValuePair> authParams=new ArrayList<>();
+        authParams.add(new BasicNameValuePair("name", userName));
+        authParams.add(new BasicNameValuePair("email",userEmail));
+        authParams.add(new BasicNameValuePair("password",userPassword));
+        request.setEntity(new UrlEncodedFormEntity(authParams));
+        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
+        String entity = EntityUtils.toString(response.getEntity());
+
+        Assert.assertTrue(entity.contains("\"email\":\""+userEmail+"\""));
+        Assert.assertTrue(entity.contains("\"name\":\""+userName+"\""));
+
+    }
+
 }
