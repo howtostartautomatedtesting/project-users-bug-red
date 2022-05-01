182110294c4a47beaf127a38cd72889825086627
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-01 17:36:00 +0300

    add test

--- src/test/java/api/CreateNewUserTest.java
+++ src/test/java/api/CreateNewUserTest.java
@@ -1,7 +1,43 @@
 package api;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.UserCreator;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *  POST http://users.bugred.ru/tasks/rest/doregister
+ * POST http://users.bugred.ru/tasks/rest/doregister
  */
 public class CreateNewUserTest {
+    private final String userName = UserCreator.getUserName();
+    private final String userEmail = UserCreator.getEmail();
+    private final String userPassword = UserCreator.getPassword();
+    private final String inCorrectEmail = "123mail.ru";
+
+    @Test
+    public void testCreateNewUserWithInCorrectEmail() throws IOException {
+        HttpPost request = new HttpPost("http://users.bugred.ru/tasks/rest/doregister");
+        List<NameValuePair> authParams = new ArrayList<>();
+        authParams.add(new BasicNameValuePair("name", userName));
+        authParams.add(new BasicNameValuePair("email", inCorrectEmail));
+        authParams.add(new BasicNameValuePair("password", userPassword));
+        request.setEntity(new UrlEncodedFormEntity(authParams));
+        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
+        String entity = EntityUtils.toString(response.getEntity());
+        Assert.assertTrue(entity.contains(inCorrectEmail));
+
+
+    }
+
 }
