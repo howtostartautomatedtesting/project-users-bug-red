0cb4232d9430ee3db307c42fcba419650a8fc473
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-03 08:58:48 +0300

    json parser

--- src/test/java/api/CreateNewUserTest.java
+++ src/test/java/api/CreateNewUserTest.java
@@ -7,12 +7,14 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.UserCreator;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,9 +26,10 @@ public class CreateNewUserTest {
     private final String userEmail = UserCreator.getEmail();
     private final String userPassword = UserCreator.getPassword();
     private final String existingEmail = "123@mail.ru";
+    private final String expectedMessageExistingEmail = " email " + existingEmail + " уже есть в базе";
 
     @Test
-    public void testCreateNewUserWithExistingEmail() throws IOException {
+    public void testCreateNewUserWithExistingEmail() throws IOException, ParseException {
         HttpPost request = new HttpPost("http://users.bugred.ru/tasks/rest/doregister");
         List<NameValuePair> authParams = new ArrayList<>();
         authParams.add(new BasicNameValuePair("name", userName));
@@ -35,9 +38,9 @@ public class CreateNewUserTest {
         request.setEntity(new UrlEncodedFormEntity(authParams));
         CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
         String entity = EntityUtils.toString(response.getEntity());
-        Assert.assertTrue(entity.contains("\"message\":\" email " + existingEmail));
-
-
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(entity);
+        String message = jsonObject.get("message").toString();
+        Assert.assertEquals(message, expectedMessageExistingEmail);
     }
-
 }
