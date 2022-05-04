949db4c39ccebbf0926dfe7ea48970aa0408c8cc
RamanBohdan <lo63iksport004@gmail.com>
2022-05-04 21:15:08 +0300

    add API test method "GetUserInfo With Missing Registered User"

--- src/test/java/api/GetUserInfo.java
+++ src/test/java/api/GetUserInfo.java
@@ -1,7 +1,53 @@
 package api;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.UserCreator;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *  GET http://users.bugred.ru/tasks/rest/getuser
+ * GET http://users.bugred.ru/tasks/rest/getuser
  */
 public class GetUserInfo {
+    private final String userName = "a";
+    private final String userEmail = "4444@mail.ru";
+    private final String userPassword = UserCreator.getPassword();
+    private final String incorrectEmail = "44441@mail.ru";
+    private final String expectedMessageUserNotFound= " Пользователь не найден" + " "+ incorrectEmail;
+
+    @Test
+    public void testGetUserInfoWithMissingRegisteredUser() throws IOException, ParseException, org.json.simple.parser.ParseException {
+        HttpPost request = new HttpPost("http://users.bugred.ru/tasks/rest/doregister");
+        List<NameValuePair> authParams = new ArrayList<>();
+        authParams.add(new BasicNameValuePair("name", userName));
+        authParams.add(new BasicNameValuePair("email", userEmail));
+        authParams.add(new BasicNameValuePair("password", userPassword));
+        request.setEntity(new UrlEncodedFormEntity(authParams));
+
+        HttpGet httpGetRequest = new HttpGet("http://users.bugred.ru/tasks/rest/getuser?email=44441@mail.ru");
+        CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpGetRequest);
+        String responseText = EntityUtils.toString(httpResponse.getEntity());
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(responseText);
+        String message = jsonObject.get("message").toString();
+
+        Assert.assertEquals(message, expectedMessageUserNotFound);
+
+        HttpClientBuilder.create().build().close();
+    }
 }
